ad83164b55b07dbf1934c5b545f0729e670f6f04
KingV5 <jeremiahelpro@gmail.com>
2026-03-09 20:28:48 +0000

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,12 +1,15 @@
 -- MainScript.lua for KingV5
--- Framework for modules
+-- Auto-loads all game modules (including universal.lua)
 
+-- Explicit arguments
 local Arguments = Arguments or {}
 local Key = Arguments.Key or "unknown key"
 
+-- Shared table for modules
 shared.vape = shared.vape or {}
 local vape = shared.vape
 
+-- Libraries placeholders
 vape.Libraries = vape.Libraries or {}
 vape.Libraries.entity = vape.Libraries.entity or {}
 vape.Libraries.targetinfo = vape.Libraries.targetinfo or {}
@@ -19,6 +22,7 @@ vape.Libraries.prediction = vape.Libraries.prediction or {}
 vape.Libraries.getfontsize = vape.Libraries.getfontsize or {}
 vape.Libraries.getcustomasset = vape.Libraries.getcustomasset or getcustomasset
 
+-- Store system
 local store = {
     attackReach = 0,
     attackReachUpdate = tick(),
@@ -29,26 +33,33 @@ local store = {
     tools = {}
 }
 
+-- Bedwars metadata placeholders
 local bedwars = {
     ItemMeta = {},
     ProjectileMeta = {}
 }
 
+-- Roblox services
 local playersService = game:GetService("Players")
 local lplr = playersService.LocalPlayer
 local runService = game:GetService("RunService")
 local collectionService = game:GetService("CollectionService")
 
+-- Module loader function
 local function loadModule(path)
+    if not isfile(path) then return end
     local code = readfile(path)
     return loadstring(code)()
 end
 
-local universalModule
-if isfile("KingV5/games/universal.lua") then
-    universalModule = loadModule("KingV5/games/universal.lua")
+-- Auto-load all modules in games/ folder
+for _, file in pairs(listfiles("KingV5/games")) do
+    if file:sub(-4) == ".lua" then
+        loadModule(file)
+    end
 end
 
+-- Export shared variables for modules
 vape.store = store
 vape.bedwars = bedwars
 vape.LocalPlayer = lplr
