89dff631cd31f324ad4c9bfda174545853f83f80
KingV5 <jeremiahelpro@gmail.com>
2026-03-09 21:02:13 +0000

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,38 +1,82 @@
--- MainScript.lua for KingV5
-local Arguments = Arguments or {}
-local Key = Arguments.Key or "unknown key"
+local Arguments = {}
 
--- Shared vape table
-shared.vape = shared.vape or {}
-local vape = shared.vape
+if script_key then
+    Arguments.Key = script_key
+else
+    Arguments.Key = "unknown key"
+end
 
--- Roblox services
-local playersService = game:GetService("Players")
-local lplr = playersService.LocalPlayer
-local runService = game:GetService("RunService")
+if shared.VapeDeveloper then
+    return loadstring(readfile("catrewrite/loader.lua"), "loader.lua")(Arguments)
+else
+    if not isfolder("catrewrite") then
+        makefolder("catrewrite")
+    end
 
--- Auto-load all game modules
-local function loadModule(path)
-    if isfile(path) then
-        local code = readfile(path)
-        local func, err = loadstring(code)
-        if func then
-            func()
-        else
-            warn("Failed to load "..path..": "..tostring(err))
+    if not isfolder("catrewrite/profiles") then
+        makefolder("catrewrite/profiles")
+    end
+
+    -- get latest commit
+    local commit = "main"
+    local suc, subbed = pcall(function()
+        return game:HttpGet("https://github.com/MaxlaserTech/CatV6")
+    end)
+
+    if suc and subbed then
+        local pos = subbed:find("currentOid")
+        if pos then
+            local possible = subbed:sub(pos + 13, pos + 52)
+            if #possible == 40 then
+                commit = possible
+            end
         end
     end
-end
 
-for _, file in pairs(listfiles("KingV5/games")) do
-    if file:sub(-4) == ".lua" then
-        loadModule(file)
+    Arguments.Commit = commit
+
+    local function downloadFile(path, func)
+        local needUpdate = not isfile(path)
+
+        if isfile("catrewrite/profiles/commit.txt") then
+            if readfile("catrewrite/profiles/commit.txt") ~= commit then
+                needUpdate = true
+            end
+        else
+            needUpdate = true
+        end
+
+        if needUpdate and not shared.VapeDeveloper then
+            local suc2, res = pcall(function()
+                local url = "https://raw.githubusercontent.com/MaxlaserTech/CatV6/"..commit.."/"..select(1, path:gsub("catrewrite/", ""))
+                return game:HttpGet(url)
+            end)
+
+            if not suc2 or not res or res == "404: Not Found" then
+                error(res or "Download failed")
+            end
+
+            if path:find(".lua") then
+                res = "--cache watermark\n"..res
+            end
+
+            writefile(path, res)
+            writefile("catrewrite/profiles/commit.txt", commit)
+        end
+
+        return (func or readfile)(path)
     end
+
+    if getconnections and not shared.catdebug then
+        for _, v in pairs(getconnections(game:GetService("ScriptContext").Error)) do
+            v:Disable()
+        end
+
+        for _, v in pairs(getconnections(game:GetService("LogService").MessageOut)) do
+            v:Disable()
+        end
+    end
+
+    shared.VapeDeveloper = Arguments.Developer
+    return loadstring(downloadFile("catrewrite/loader.lua"), "loader.lua")(Arguments)
 end
-
--- Export shared variables for modules
-vape.LocalPlayer = lplr
-vape.Players = playersService
-vape.RunService = runService
-
-print("KingV5 main script loaded successfully!")
