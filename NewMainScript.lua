fd6c76dd69b19cfd2fd81c2f800eb265de06a4cd
KingV5 <jeremiahelpro@gmail.com>
2026-03-09 16:59:37 +0000

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,55 +1,82 @@
-local Arguments = ... or {}
-if not Arguments.Key then
-    Arguments.Key = script_key or 'unknown key'
+local Arguments = {}
+
+if script_key then
+    Arguments.Key = script_key
+else
+    Arguments.Key = "unknown key"
 end
 
 if shared.VapeDeveloper then
-    return loadstring(readfile('catrewrite/loader.lua'), 'loader.lua')(Arguments)
+    return loadstring(readfile("catrewrite/loader.lua"), "loader.lua")(Arguments)
 else
-    if not isfolder('catrewrite') then
-        makefolder('catrewrite')
+    if not isfolder("catrewrite") then
+        makefolder("catrewrite")
     end
 
-    if not isfolder('catrewrite/profiles') then
-        makefolder('catrewrite/profiles')
+    if not isfolder("catrewrite/profiles") then
+        makefolder("catrewrite/profiles")
     end
 
-    local _, subbed = pcall(function()
-        return game:HttpGet('https://github.com/MaxlaserTech/CatV6')
+    -- get latest commit
+    local commit = "main"
+    local suc, subbed = pcall(function()
+        return game:HttpGet("https://github.com/MaxlaserTech/CatV6")
     end)
 
-    local commit = subbed:find('currentOid')
-    commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-    commit = commit and #commit == 40 and commit or 'main'
+    if suc and subbed then
+        local pos = subbed:find("currentOid")
+        if pos then
+            local possible = subbed:sub(pos + 13, pos + 52)
+            if #possible == 40 then
+                commit = possible
+            end
+        end
+    end
+
     Arguments.Commit = commit
 
     local function downloadFile(path, func)
-        if not isfile(path) or (not isfile('catrewrite/profiles/commit.txt') or readfile('catrewrite/profiles/commit.txt') ~= commit) and not shared.VapeDeveloper then
-            local suc, res = pcall(function()
-                return game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/'.. commit.. '/' ..select(1, path:gsub('catrewrite/', '')), true)
-            end)
-            print('updated')
-            if not suc or res == '404: Not Found' then
-                error(res)
+        local needUpdate = not isfile(path)
+
+        if isfile("catrewrite/profiles/commit.txt") then
+            if readfile("catrewrite/profiles/commit.txt") ~= commit then
+                needUpdate = true
             end
-            if path:find('.lua') then
-                res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
-            end
-            writefile(path, res)
+        else
+            needUpdate = true
         end
+
+        if needUpdate and not shared.VapeDeveloper then
+            local suc2, res = pcall(function()
+                local url = "https://raw.githubusercontent.com/MaxlaserTech/CatV6/"..commit.."/"..select(1, path:gsub("catrewrite/", ""))
+                return game:HttpGet(url)
+            end)
+
+            if not suc2 or not res or res == "404: Not Found" then
+                error(res or "Download failed")
+            end
+
+            if path:find(".lua") then
+                res = "--cache watermark\n"..res
+            end
+
+            writefile(path, res)
+            writefile("catrewrite/profiles/commit.txt", commit)
+        end
+
         return (func or readfile)(path)
     end
 
     if getconnections and not shared.catdebug then
-        for _, v in getconnections(cloneref(game:GetService('ScriptContext')).Error) do
+        for _, v in pairs(getconnections(game:GetService("ScriptContext").Error)) do
             v:Disable()
         end
 
-        for _, v in getconnections(cloneref(game:GetService('LogService')).MessageOut) do
+        for _, v in pairs(getconnections(game:GetService("LogService").MessageOut)) do
             v:Disable()
         end
     end
 
     shared.VapeDeveloper = Arguments.Developer
-    return loadstring(downloadFile('catrewrite/loader.lua'), 'loader.lua')(Arguments)
+    return loadstring(downloadFile("catrewrite/loader.lua"), "loader.lua")(Arguments)
 end
