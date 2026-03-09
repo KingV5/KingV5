3611cfc7322494643ea6d3a4f8120b3d653dbe5b
KingV5 <jeremiahelpro@gmail.com>
2026-03-09 21:57:04 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,49 +1,28 @@
-local isfile = isfile or function(file)
-	local suc, res = pcall(function()
-		return readfile(file)
-	end)
-	return suc and res ~= nil and res ~= ""
+local function getGithubFile(path)
+	return game:HttpGet("https://raw.githubusercontent.com/KingV5/KingV5/main/"..path, true)
 end
 
-local function downloadFile(path)
-	local data
-
-	if not isfile(path) then
-		local suc, res = pcall(function()
-			return game:HttpGet(
-				"https://raw.githubusercontent.com/KingV5/KingV5/main/" ..
-				select(1, path:gsub("KingV5/", "")),
-				true
-			)
-		end)
-
-		if not suc or res == "404: Not Found" then
-			error("Failed to download "..path)
-		end
-
-		data = res
-		writefile(path, res)
-	else
-		data = readfile(path)
-	end
-
-	return data
-end
-
--- create folders
-for _, folder in {
-	"KingV5",
-	"KingV5/games",
-	"KingV5/profiles",
-	"KingV5/assets",
-	"KingV5/libraries",
-	"KingV5/guis"
-} do
+local function ensureFolder(folder)
 	if not isfolder(folder) then
 		makefolder(folder)
 	end
 end
 
--- download + run universal.lua
-local scriptData = downloadFile("KingV5/games/universal.lua")
-return loadstring(scriptData, "universal")()
+local function saveFile(path, data)
+	writefile(path, data)
+end
+
+-- folders
+ensureFolder("KingV5")
+ensureFolder("KingV5/games")
+ensureFolder("KingV5/libraries")
+ensureFolder("KingV5/assets")
+ensureFolder("KingV5/guis")
+ensureFolder("KingV5/profiles")
+
+-- download universal
+local universal = getGithubFile("games/universal.lua")
+saveFile("KingV5/games/universal.lua", universal)
+
+-- run universal
+loadstring(universal)()
