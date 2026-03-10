cad3eacca2604f845be340b8a97922b1e79f289b
KingV5 <jeremiahelpro@gmail.com>
2026-03-10 10:34:05 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,7 @@
+-- i love whitepine! w whitepine!
+local license = ...
 repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
-
--- why do exploits fail to implement anything correctly? Is it really that hard?
-if identifyexecutor then
-	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
-		getgenv().setthreadidentity = nil
-	end
-end
 
 local vape
 local loadstring = function(...)
@@ -27,11 +22,16 @@ local cloneref = cloneref or function(obj)
 	return obj
 end
 local playersService = cloneref(game:GetService('Players'))
+local httpService = cloneref(game:GetService('HttpService'))
 
 local function downloadFile(path, func)
+	local downloader = getgenv().catdownloader
+	if downloader then
+		downloader.Text = `Downloading {path}`
+	end
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/'..readfile('catrewrite/profiles/commit.txt')..'/'..select(1, path:gsub('catrewrite/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -41,29 +41,55 @@ local function downloadFile(path, func)
 		end
 		writefile(path, res)
 	end
+	if downloader then
+		downloader.Text = ''
+	end
 	return (func or readfile)(path)
+end
+shared.catdata = license
+
+local function compileTable(tab)
+	local json = '{'
+	for i, v in tab do
+		json = `{json}\n					    {i} = {typeof(v) == 'string' and '"'.. v.. '"' or v},`
+	end
+	return `{json}\n					}`
 end
 
 local function finishLoading()
 	vape.Init = nil
 	vape:Load()
-	task.spawn(function()
+	vape:Clean(task.spawn(function()
 		repeat
-			vape:Save()
+			pcall(function() vape:Save() end)
 			task.wait(10)
-		until not vape.Loaded
-	end)
+			local suc, commit = pcall(function()
+				local _, subbed = pcall(function()
+					return game:HttpGet('https://github.com/MaxlaserTech/CatV6')
+				end)
+				local commit = subbed:find('currentOid')
+				commit = commit and subbed:sub(commit + 13, commit + 52) or nil
+				commit = commit and #commit == 40 and commit or 'main'
+				return commit
+			end)
+			if suc and commit then
+				if readfile('catrewrite/profiles/commit.txt') ~= commit then
+					vape:CreateNotification('Cat', 'Cat Vape has updated! Please re-execute the script to get the changes', 5, 'info')
+				end
+			end
+		until false
+	end))
 
 	local teleportedServers
 	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-		if (not teleportedServers) and (not shared.VapeIndependent) then
+		if (not teleportedServers) and (not shared.VapeIndependent) and vape.AutoTeleport.Enabled then
 			teleportedServers = true
 			local teleportScript = [[
 				shared.vapereload = true
 				if shared.VapeDeveloper then
-					loadstring(readfile('newvape/loader.lua'), 'loader')()
+					loadstring(readfile('catrewrite/loader.lua'), 'loader')(sharedData)
 				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+					loadstring(game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/'..readfile('catrewrite/profiles/commit.txt')..'/loader.lua', true), 'loader')(sharedData)
 				end
 			]]
 			if shared.VapeDeveloper then
@@ -72,6 +98,7 @@ local function finishLoading()
 			if shared.VapeCustomProfile then
 				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
 			end
+			teleportScript = teleportScript:gsub('sharedData', compileTable(license))
 			vape:Save()
 			queue_on_teleport(teleportScript)
 		end
@@ -79,37 +106,51 @@ local function finishLoading()
 
 	if not shared.vapereload then
 		if not vape.Categories then return end
+		
+		if shared.maincat then
+			vape:CreateNotification('Cat', 'Your using an outdated loader of catvape, Get new one at discord.gg/catvape', 30, 'info')
+			shared.maincat = nil
+		end
+		vape:CreateNotification('Cat', 'Our last server got limited, Join our backup server discord.gg/catvape', 30, 'warning')
+		
 		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
+			task.wait(0.5)
 			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
+			task.wait(0.5)
+			vape:CreateNotification('Cat', `Initalized as {getgenv().catname} with {getgenv().catrole}`, 5, 'info')
 		end
 	end
 end
 
-if not isfile('newvape/profiles/gui.txt') then
-	writefile('newvape/profiles/gui.txt', 'new')
+if not isfile('catrewrite/profiles/gui.txt') then
+	writefile('catrewrite/profiles/gui.txt', 'new')
 end
-local gui = readfile('newvape/profiles/gui.txt')
+local gui = readfile('catrewrite/profiles/gui.txt')
 
-if not isfolder('newvape/assets/'..gui) then
-	makefolder('newvape/assets/'..gui)
+if not isfolder('catrewrite/assets/'..gui) then
+	makefolder('catrewrite/assets/'..gui)
 end
-vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
+vape = loadstring(downloadFile('catrewrite/guis/'..gui..'.lua'), 'gui')()
 shared.vape = vape
+_G.vape = shared.vape or {'?'}
 
 if not shared.VapeIndependent then
-	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
-	if isfile('newvape/games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+	loadstring(downloadFile('catrewrite/games/universal.lua'), 'universal')()
+	if isfile('catrewrite/games/'..game.PlaceId..'.lua') then
+		loadstring(readfile('catrewrite/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
 	else
 		if not shared.VapeDeveloper then
-			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+			local success, result = pcall(function()
+				return game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/'..readfile('catrewrite/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua')
 			end)
-			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+
+			if success and result ~= '404: Not Found' then
+				writefile(`catrewrite/games/{game.PlaceId}.lua`, result)
+				loadstring(result, tostring(game.PlaceId))(...)
 			end
 		end
 	end
+	loadstring(downloadFile('catrewrite/libraries/script.lua'), 'script.lua')(license.Key)
 	finishLoading()
 else
 	vape.Init = finishLoading
